408a20c7b9c201247d81c00e5fb282f9f7dab346
jfernand <jfernand196@gmail.com>
2022-01-24 22:15:11 -0500

    chore: repit text input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+// const [count, setCount] = useState(0)
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Un repetidor</h1>
+//         <input type="text" placeholder="Empieza a escribir algo" onChange={setState(e.target.value)}/>
+//         <p className="repeater">{count}</p>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => {
+
+  const [count, setCount] = useState('')  
+  return (
+    <div>
+      <h1>Un repetidor</h1>
+      <input
+        type="text"
+        placeholder="Empieza a escribir algo"
+        onChange={e => setCount(e.target.value)}
+      />
+      <p className="repeater">{count}</p>
+    </div>
+  );
+};
 
 export default App;
